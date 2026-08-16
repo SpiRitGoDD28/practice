6de2cc96dac9138172ac981ecf2ac916e2a3886b
SpiRitGoDD28 <28sujalshaw@gmail.com>
2026-08-16 21:28:18 +0530

    Time: 5 ms (25.5%), Space: 9.9 MB (26.25%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        unordered_map<char, int>sMap;
+        unordered_map<char, int>tMap;
+        int i=0;
+        for(auto ch: s){
+            sMap[ch]++;
+        }
+        for(auto ch: t){
+            tMap[ch]++;
+        }
+        if(sMap!=tMap){
+            return false;
+        }
+
+        return true;
+
+    }
+};
