e04a2b124e5d4fc483a656e01f533aef91505c1e
SpiRitGoDD28 <28sujalshaw@gmail.com>
2026-08-19 16:00:36 +0530

    Time: 0 ms (100%), Space: 13.8 MB (49.03%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -3,25 +3,24 @@ public:
     int compress(vector<char>& chars) {
         int n=chars.size();
         int index=0;
-        for(int i=0; i<n; i++){
-            char ch= chars[i];
+        for(int i=0;i<n;i++){
             int count=0;
-            while(i<n && chars[i]==ch){
+            char words= chars[i];
+            while(i<n && chars[i]==words){
                 count++;
                 i++;
             }
             if(count==1){
-                chars[index++]=ch;
+                chars[index++]=words;
             }
             else{
-                chars[index++]=ch;
-                string str= to_string(count);
-                for(char dig: str){
-                    chars[index++]=dig;
+                chars[index++]=words;
+                string str;
+                str=to_string(count);
+                for(char digit: str){
+                    chars[index++]=digit;
                 }
-                
             }
-            
             i--;
         }
         return index;
