69253b6362680c23ceb7822f3ff2d8a6b52a80de
SpiRitGoDD28 <28sujalshaw@gmail.com>
2026-08-23 06:51:32 +0530

    Time: 0 ms (100%), Space: 9.1 MB (8.6%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n=s.size();
-        string lastWord;
+        //string lastWord;
         int end=0;
         int i=n-1;
             while(i>=0 && s[i]==' '){
@@ -13,9 +13,10 @@ public:
             while(i>=0 && s[i]!=' '){
                 i--;
             }
-            lastWord=s.substr(i+1, end-i);
+            //lastWord=s.substr(i+1, end-i);
         
 
-        return lastWord.size();
+        //return lastWord.size();
+        return end-i;
     }
 };
